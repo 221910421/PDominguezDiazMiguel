35160f9ec7cda6607e607f32283388335489682e
Miguel Betzay Dominguez Diaz <al221910421@gmail.com>
2022-07-02 09:45:39 -0500

    Instalación manual de iconos

--- App.js
+++ App.js
@@ -2,19 +2,24 @@ import * as React from 'react';
 import { Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 function HomeScreen() {
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Home!</Text>
+      <Text>Home</Text>
+      <Text><Icon name="home" size={30} color="#900" /></Text>
     </View>
   );
 }
+
+
 
 function SettingsScreen() {
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
       <Text>Settings!</Text>
+      <Text><Icon name="bell" size={30} color="#900" /></Text>
     </View>
   );
 }
@@ -25,8 +30,8 @@ export default function App() {
   return (
     <NavigationContainer>
       <Tab.Navigator>
-        <Tab.Screen name="Home" component={HomeScreen} />
-        <Tab.Screen name="Settings" component={SettingsScreen} />
+        <Tab.Screen name="Home" component={HomeScreen}/>
+        <Tab.Screen name="Settings" component={SettingsScreen}/>
       </Tab.Navigator>
     </NavigationContainer>
   );
